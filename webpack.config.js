d8306ba26dc08d14cc3a458859a7e487157c53c3
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-28 19:45:09 +0200

    f

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(tsx?|jsx?)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
